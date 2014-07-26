a9c59b2398a1a8ab8a0b03d02ea68538e28bac6d
CalciumScript <cyril.charlier@outlook.com>
2014-07-26 07:05:13 +0200

    Revert to 2014.717.1

--- script.js
+++ script.js
@@ -1,50 +1,6 @@
-// ==UserScript==
-// @name		DoA Power Tools Plus V (modded by Calcium)
-// @namespace	http://userscripts.org:8080/scripts/show/173473
-// @icon		http://img66.xooimage.com/files/3/6/d/jaws64-2b5041c.png
-// @description	Enhanced Power Tools for Dragons of Atlantis, modded by Calcium - Supported languages : Dutch, English, French, German, Spanish, Swedish
-// @grant		GM_xmlhttpRequest
-// @grant		GM_info
-// @grant		GM_setValue
-// @grant		GM_getValue
-// @match		*://apps.facebook.com/dragonsofatlantis/*
-// @match		*://*.castle.wonderhill.com/platforms/*/game
-// @match		*://www.kabam.com/dragons-of-atlantis/play
-// @match		*://www.kongregate.com/games/kabam/dragons-of-atlantis
-// @match		*://plus.google.com/games/659749063556*
-// @match		*://*.googleusercontent.com/gadgets/ifr?url=app://659749063556*
-// @include		*://plus.google.com/games/659749063556*
-// @include		*://plus.google.com/*/games/659749063556*
-// @include		*://www.kongregate.com/games/kabam/dragons-of-atlantis*
-// @include		*://*.castle.wonderhill.com/platforms/*/game
-// @include		*://*.googleusercontent.com/gadgets/ifr?url=app://659749063556*
-// @include		*://www.kabam.com/dragons-of-atlantis/play
-// @include		*://apps.facebook.com/dragonsofatlantis/*
-// @include		*://*.castle.wonderhill.com/platforms/*/game
-// @exclude		*://apps.facebook.com/dragonsofatlantis/rubies
-// @exclude		*://apps.facebook.com/ai.php*
-// @exclude		*://www.facebook.com/plugins/like.php*
-// @exclude		*://kabam1-a.akamaihd.net/pixelkabam/*
-// @exclude		*://*.akamaihd.net/pixelkabam/*
-// @exclude		*://plus.google.com/_/apps-static/*
-// @exclude		*://plus.google.com/u/0/_/gadgets/contactPicker*
-// @exclude		*://accounts.google.com/*
-// @exclude		*://talkgadget.google.com/*
-// @exclude		*://www.googleapis.com/static*
-// @include		*://*realm*doa.altervista.org*
-// @exclude		*://realmtheraindoa.altervista.org/Jeux/*
-// @version		2014.717.1
-// @changeLog	More and more....
-// ==/UserScript== 
-
-/*
-	===== TO DO EVERYTIME WHEN PACKAGING CHROME EXTENSION =====
-	!! Change CHROME_EXT here and version in manifest.json before packing !!
-*/
-
 (function() {
 
-    var CHROME_EXT = true, scriptVersion = '2014.717.1', scriptId = '173473', REALM_URL = '', REALM_NAME, chrome_extensions = 'chrome://chrome/extensions/', userscripts_src = 'http://userscripts.org:8080/scripts/source/' + scriptId + '.user.js', UID = {}, UIDN = {}, REMOVE_HD = false;
+    var CHROME_EXT = true, scriptVersion = '2014.717.2', scriptId = '173473', REALM_URL = '', REALM_NAME, chrome_extensions = 'chrome://chrome/extensions/', userscripts_src = 'http://userscripts.org:8080/scripts/source/' + scriptId + '.user.js', UID = {}, UIDN = {}, REMOVE_HD = false;
 
 	function make_space_for_kongregate(frame, width) {
 		var maxWidth = (width ? width : (document.body.offsetWidth - 50) + 'px');
